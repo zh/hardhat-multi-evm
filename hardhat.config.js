2cf123b8a01d2d29c8c7b459799e1f8d95f6462f
zhware <zh@zhware.net>
2021-10-04 21:03:03 +0900

    Support for different EVM-compatible blockchains

--- hardhat.config.js
+++ hardhat.config.js
@@ -14,9 +14,10 @@ const infuraKey = process.env.INFURA_KEY;
 
 const smartbchFee = 1050000000;
 
-function mnemonic() {
+function mnemonic(network="mainnet") {
   try {
-    return fs.readFileSync("./mnemonic.txt").toString().trim();
+    const fileName = network === "mainnet" ? "mnemonic" : `mnemonic_${network}`
+    return fs.readFileSync(`./${fileName}.txt`).toString().trim();
   } catch (e) {
     if (defaultNetwork !== "localhost") {
       console.log(
@@ -45,15 +46,64 @@ module.exports = {
     testnetSmartBCH: {
       url: "http://35.220.203.194:8545", // "https://moeing.tech:9545",
       chainId: 10001,
-      gasPrice: smartbchFee,
+      gasPrice: 1050000000,
       accounts: {
-        mnemonic: mnemonic()
+        mnemonic: mnemonic("testnet")
       }
     },
     mainnetSmartBCH: {
       url: "https://smartbch.greyh.at", // "https://global.uat.cash",
       chainId: 10000,
-      gasPrice: smartbchFee,
+      gasPrice: 1050000000,
+      accounts: {
+        mnemonic: mnemonic()
+      }
+    },
+    localAva: {
+      url: 'http://localhost:9650/ext/bc/C/rpc',
+      gasPrice: 225000000000,
+      chainId: 43112,
+      accounts: [
+        "0x56289e99c94b6912bfc12adc093c9b51124f0dc54ac7a766b2bc5ccf558d8027",
+        "0x7b4198529994b0dc604278c99d153cfd069d594753d471171a1d102a10438e07",
+        "0x15614556be13730e9e8d6eacc1603143e7b96987429df8726384c2ec4502ef6e",
+        "0x31b571bf6894a248831ff937bb49f7754509fe93bbd2517c9c73c4144c0e97dc",
+        "0x6934bef917e01692b789da754a0eae31a8536eb465e7bff752ea291dad88c675",
+        "0xe700bdbdbc279b808b1ec45f8c2370e4616d3a02c336e68d85d4668e08f53cff",
+        "0xbbc2865b76ba28016bc2255c7504d000e046ae01934b04c694592a6276988630",
+        "0xcdbfd34f687ced8c6968854f8a99ae47712c4f4183b78dcc4a903d1bfe8cbf60",
+        "0x86f78c5416151fe3546dece84fda4b4b1e36089f2dbc48496faf3a950f16157c",
+        "0x750839e9dbbd2a0910efe40f50b2f3b2f2f59f5580bb4b83bd8c1201cf9a010a"
+      ]
+    },
+    fujiAva: {
+      url: 'https://api.avax-test.network/ext/bc/C/rpc',
+      gasPrice: 225000000000,
+      chainId: 43113,
+      accounts: {
+        mnemonic: mnemonic("testnet")
+      }
+    },
+    mainnetAva: {
+      url: 'https://api.avax.network/ext/bc/C/rpc',
+      gasPrice: 225000000000,
+      chainId: 43114,
+      accounts: {
+        mnemonic: mnemonic()
+      }
+    },
+    testnetFantom: {
+      url: 'https://rpc.testnet.fantom.network/',
+      gasPrice: 1800000000,
+      chainId: 4002,
+      accounts: {
+        mnemonic: mnemonic("testnet")
+      }
+    },
+    fantomOpera: {
+      url: 'https://rpc.ftm.tools/',
+      gasPrice: 1600000000,
+      chainId: 250,
       accounts: {
         mnemonic: mnemonic()
       }
@@ -63,6 +113,24 @@ module.exports = {
     compilers: [
       {
         version: "0.8.4",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.8.1",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.7.4",
         settings: {
           optimizer: {
             enabled: true,
@@ -86,7 +154,11 @@ module.exports = {
       default: 0, // here this will by default take the first account as deployer
       'kovan': deployerAddress,
       'mainnetSmartBCH': deployerAddress,
-      'testnetSmartBCH': deployerAddress
+      'testnetSmartBCH': deployerAddress,
+      'fujiAva': deployerAddress,
+      'mainnetAva': deployerAddress,
+      'testnetFantom': deployerAddress,
+      'fantomOpera': deployerAddress
     },
   }
 };
@@ -202,4 +274,4 @@ task("balance", "Prints an account's balance")
       await addr(ethers, taskArgs.account)
     );
     console.log(formatUnits(balance, "ether"), "ETH");
-  });
+});
